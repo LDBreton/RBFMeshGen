22bc0902b716cbea1b466c0eca39e082012c89ef
Louis.D Breton <louis.breton@ciencias.unam.mx>
2024-05-02 00:53:15 -0600

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RBFMeshGen",
-    version="1.0.4",
+    version="1.0.5",
     author="Louis Breton",
     author_email="louis.breton@ciencias.unam.mx",
     description="A Python package for generating random meshes based on radial basis functions.",
